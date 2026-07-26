729a208aa4e3aed367b6667d7519e3537e216068
pavanmankala <pavankumarmvs27@gmail.com>
2026-07-26 11:31:32 +0530

    Change EC2 instance type from t2.micro to t3.micro

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   ami                         = "ami-0c55b159cbfafe1f0"   # your AMI
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
